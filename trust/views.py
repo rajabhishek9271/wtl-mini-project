e89a362ed392639d4b1d1fc2caabb0c8866665b7
rajabhishek9271 <rajabhishek9271@gmail.com>
2021-01-12 16:11:00 +0530

    Django backend setup done

---
+++ trust/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+
+
+# Create your views here.
+class HomePage(View):
+    def get(self, request, *args, **kwargs):
+
+        return render(request, 'index.html')
+
+
+class AboutPage(View):
+    def get(self, request, *args, **kwargs):
+
+        return render(request, 'about.html')
+
+
+class ContactPage(View):
+    def get(self, request, *args, **kwargs):
+
+        return render(request, 'contact.html')
+
+
+class GalleryPage(View):
+    def get(self, request, *args, **kwargs):
+
+        return render(request, 'gallery.html')
+
+
+class VolunteerPage(View):
+    def get(self, request, *args, **kwargs):
+
+        return render(request, 'gallery.html')
+
+
+class CausesPage(View):
+    def get(self, request, *args, **kwargs):
+
+        return render(request, 'causes.html')
